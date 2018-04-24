c25ddb115ebc29ab7a02ab79b91edc05740a2613
Florah Masakhwe <masakhweflosh@gmail.com>
2018-04-24 16:21:46 +0300

    add variables and functions to the file

--- js/scripts.js
+++ js/scripts.js
@@ -2,14 +2,12 @@ var add = function(number1, number2) {
       return number1 + number2;
     };
 
-  alert(add(10,5));
 
       var add = function(number1, number2){
       return number1 + number2;
       };
 
-        var number1 = parseInt(prompt("Enter a number:"));
+      var number1 = parseInt(prompt("Enter a number:"));
       var number2 = parseInt(prompt("Enter another number:"));
 
-      alert(add(number1, number2));
-    
+      alert(result);
